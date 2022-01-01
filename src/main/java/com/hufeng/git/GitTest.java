85bbdbc528852acc9b8e8bde1d653e286a8786fa
hufeng <hufeng0101@foxmail.com>
2022-01-01 20:43:27 +0800

    first commit

---
+++ src/main/java/com/hufeng/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.hufeng.git;
+
+/**
+ * @Author HuFeng
+ * @Date 2022/1/1 20:39:10
+ * @Description:
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
